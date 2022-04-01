ca5e4a04e5f9fb06858f8a43e97538ab60e1a6a9
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-01 12:18:25 +0300

    Task class

--- TaskGenerator/TaskGenerator/Form1.Designer.cs
+++ TaskGenerator/TaskGenerator/Form1.Designer.cs
@@ -28,13 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- TaskGenerator/TaskGenerator/Form1.cs
+++ TaskGenerator/TaskGenerator/Form1.cs
@@ -10,10 +10,28 @@ using System.Windows.Forms;
 
 namespace TaskGenerator
 {
+    public class Task
+    {
+        public string number; //?
+        public string condition;
+        public List<string> questions;
+        public List<string> answers;
+
+        public virtual void Decision()
+        {
+
+        }
+        public Task()
+        {
+            questions = new List<string>();
+            answers = new List<string>();
+        }
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
+            Console.WriteLine("TaskGeneratorProbabilityTheory");
             InitializeComponent();
         }
     }
