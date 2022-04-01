59afc997de64ae56d121e2da958fbda85a2e52f7
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-01 12:37:06 +0300

    Task1 example

--- TaskGenerator/TaskGenerator/Form1.cs
+++ TaskGenerator/TaskGenerator/Form1.cs
@@ -19,7 +19,7 @@ namespace TaskGenerator
 
         public virtual void Decision()
         {
-
+            //Тут по идее генерация рандомных значений для условие, метод решения задачи и заполнение полей. 
         }
         public Task()
         {
@@ -27,11 +27,28 @@ namespace TaskGenerator
             answers = new List<string>();
         }
     }
+
+    public class Task1 : Task
+    {
+        public override void Decision()
+        {
+            number = "1";
+            condition = "Наугад выбирается автомобиль с четырехзначным номером. Найти вероятность того, что: ";
+            questions.Add("Это автомобиль Ф. Киркорова");
+            questions.Add("Номер не содержит одинаковых цифр.");
+            answers.Add("1/10000"); //?
+            answers.Add("5040/10000"); //?
+            Console.WriteLine("Task " + number + ". \n" + condition + "\n" + "a) " + questions[0] + "\n" + "б) " + questions[1] + "\n" +
+                              "Answers: \n" + "a) " + answers[0] + "\n" + "б) " + answers[1] + "\n");
+        }
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
             Console.WriteLine("TaskGeneratorProbabilityTheory");
+            Task1 task1 = new Task1();
+            task1.Decision();
             InitializeComponent();
         }
     }
