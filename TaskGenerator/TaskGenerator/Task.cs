80b313e093aa821f98f2b2d0b215428ad555c084
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-01 18:44:52 +0300

    Task перенесён в отдельный файл

---
+++ TaskGenerator/TaskGenerator/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskGenerator
+{
+    //Очень много букав, пришлось вынести в отдельный файл
+    public class Task
+    {
+        public string number; //?
+        public string condition;
+        public List<string> questions;
+        public List<string> answers;
+
+        public Task()
+        {
+            questions = new List<string>();
+            answers = new List<string>();
+        }
+    }
+}
