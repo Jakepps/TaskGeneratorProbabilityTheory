0c7172fc4e071ae89b404d33f1fcaa68d83276f7
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-03 17:52:50 +0300

    Import fix
    Метод ввода теперь не статический. Метод GetTaskTypesFromString перенесён в класс Import.

--- TaskGenerator/TaskGenerator/Structure/Import.cs
+++ TaskGenerator/TaskGenerator/Structure/Import.cs
@@ -10,20 +10,61 @@ namespace TaskGenerator
 
     public class Import
     {
-        public List<string> studentName = new List<string>();
-        public static void Op()
+        public List<string> studentName;
+        public void Op()
         {   //открытие файл, я хз как его читать без полного пути, как мне показывал рустам не получается
             Application application = new();
             Document document = application.Documents.Open("C:\\Users\\artem\\source\\repos\\Jakepps\\TaskGeneratorProbabilityTheory\\TaskGenerator\\a.docx");
 
+            string[] chars = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ")", "\r" };
             int count = document.Words.Count;
             for (int i = 1; i <= count; i++)
             { 
                 string text = document.Words[i].Text;
-                if (text == "1" || text == "2" || text == "3" || text == "4" || text == "5" || text == "6" || text == "7" || text == "8" || text == "9" || text == "0" || text == ")" || text == "\r") continue;
-               // else studentName.Add(text);   
+                if (chars.Contains(text)) continue;
+                studentName.Add(text);   
             }
             application.Quit();
+        }
+
+        public static List<int> GetTaskTypesFromString(String str)
+        {
+            List<int> result = new List<int>();
+
+            char[] chars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '-' };
+            StringBuilder sbString = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (chars.Contains(str[i]))
+                    sbString.Append(str[i]);
+            }
+
+            String clearedString = sbString.ToString();
+            String[] splittedString = clearedString.Split(new char[] { ',' });
+
+            for (int i = 0; i < splittedString.Length; i++)
+            {
+                if (splittedString[i].Contains('-'))
+                {
+                    String[] splittedString2 = splittedString[i].Split('-');
+                    int from = Convert.ToInt32(splittedString2[0]), to = Convert.ToInt32(splittedString2[1]);
+                    for (int j = from; j <= to; j++)
+                    {
+                        result.Add(j);
+                    }
+                }
+                else
+                {
+                    int type = Convert.ToInt32(splittedString[i]);
+                    result.Add(type);
+                }
+            }
+            return result;
+        }
+
+        public Import()
+        {
+            studentName = new List<string>();
         }
     }
 
--- TaskGenerator/TaskGenerator/Structure/Variant.cs
+++ TaskGenerator/TaskGenerator/Structure/Variant.cs
@@ -60,42 +60,6 @@ namespace TaskGenerator
             Console.WriteLine();
         }
 
-        //Перенести в класс импорта данных, когда он будет готов.
-        public static List<int> GetTaskTypesFromString(String str)
-        {
-            List<int> result = new List<int>();
-
-            char[] chars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '-' };
-            StringBuilder sbString = new StringBuilder();
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (chars.Contains(str[i]))
-                    sbString.Append(str[i]);
-            }
-
-            String clearedString = sbString.ToString();
-            String[] splittedString = clearedString.Split(new char[] { ',' });
-
-            for (int i = 0; i < splittedString.Length; i++)
-            {
-                if (splittedString[i].Contains('-'))
-                {
-                    String[] splittedString2 = splittedString[i].Split('-');
-                    int from = Convert.ToInt32(splittedString2[0]), to = Convert.ToInt32(splittedString2[1]);
-                    for (int j = from; j <= to; j++)
-                    {
-                        result.Add(j);
-                    }
-                }
-                else
-                {
-                    int type = Convert.ToInt32(splittedString[i]);
-                    result.Add(type);
-                }
-            }
-            return result;
-        }
-
 
         public Variant(List<int> taskTypes)
         {
