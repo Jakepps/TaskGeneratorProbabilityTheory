b31d7719eb35bcdbf3acdce2a745529a03cb4bc9
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-04-03 17:28:14 +0300

    GenerateSomeVariants
    Метод генерации заданного количества вариантов. Теперь в Variant не хранится имя студента.

--- TaskGenerator/TaskGenerator/Structure/Variant.cs
+++ TaskGenerator/TaskGenerator/Structure/Variant.cs
@@ -9,7 +9,7 @@ namespace TaskGenerator
     public class Variant
     {
         public int number;
-        public string student;
+        //public string student;
         public List<Task> tasks;
 
         public void RegenerateTaskSubtype(int index)
@@ -23,23 +23,38 @@ namespace TaskGenerator
             if(index < 0 || index >= tasks.Count) throw new ArgumentOutOfRangeException("index");
             tasks[index] = TaskConstructor.CreateTask(tasks[index].type, tasks[index].subtype);
         }
-        public static List<Variant> GetVariantsForStudents(List<String> students, List<int> taskTypes)
+
+        public static List<Variant>  GenerateSomeVariants(int count, List<int> taskTypes)
         {
             List<Variant> result = new List<Variant>();
-            for (int i = 0; i < students.Count; i++)
+            for (int i = 0; i < count; i++)
             {
                 Variant var = new Variant(taskTypes)
                 {
-                    number = i + 1,
-                    student = students[i]
+                    number = i + 1
                 };
                 result.Add(var);
-            } 
+            }
             return result;
         }
+        //public static List<Variant> GetVariantsForStudents(List<String> students, List<int> taskTypes)
+        //{
+        //    List<Variant> result = new List<Variant>();
+        //    for (int i = 0; i < students.Count; i++)
+        //    {
+        //        Variant var = new Variant(taskTypes)
+        //        {
+        //            number = i + 1,
+        //            student = students[i]
+        //        };
+        //        result.Add(var);
+        //    } 
+        //    return result;
+        //}
+
         public void TestPrint()
         {
-            Console.WriteLine(number + "-В " + student);
+            Console.WriteLine(number + "-В ");
             foreach (Task task in tasks)
                 task.TestPrint();
             Console.WriteLine();
