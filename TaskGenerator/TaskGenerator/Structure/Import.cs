aa19e93153046b7b808d98c153d50d8c82e248e6
Jakepps <nagalevsiy@mail.ru>
2022-04-04 23:09:04 +0300

    Update Import.cs

--- TaskGenerator/TaskGenerator/Structure/Import.cs
+++ TaskGenerator/TaskGenerator/Structure/Import.cs
@@ -18,7 +18,6 @@ namespace TaskGenerator
             Application application = new();
             Document document = application.Documents.Open("C:\\test\\a.docx");
             char[] chars = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ')', '(','*','-' };
-            //string[] chars = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ")", "\r" };
             int count = document.Words.Count;
             string[] arrString = new string[count + 1];
             for (int i = 1; i <= count; i++)
