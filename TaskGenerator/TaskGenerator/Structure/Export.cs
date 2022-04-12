b454b0c28e3876381f106d55c3a86fcf43d4b36c
Jakepps <nagalevsiy@mail.ru>
2022-04-12 11:09:53 +0300

    Экспорт

---
+++ TaskGenerator/TaskGenerator/Structure/Export.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word= Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace TaskGenerator.Structure
+{
+    internal class Export
+    {
+    }
+}
