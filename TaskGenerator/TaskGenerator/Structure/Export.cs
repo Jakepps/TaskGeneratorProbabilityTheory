ecb2a4b80dd215a7f0b894ac8ad270d7687eea52
Jakepps <nagalevsiy@mail.ru>
2022-04-19 21:34:39 +0300

    Update Export.cs

--- TaskGenerator/TaskGenerator/Structure/Export.cs
+++ TaskGenerator/TaskGenerator/Structure/Export.cs
@@ -18,64 +18,26 @@ namespace TaskGenerator
             {
                 string title = variantList[i].student;
                 string titlevar = "Вариант " + (i + 1);
-                //Formatting Title  
                 Formatting titleFormat = new Formatting();
-                //Specify font family  
                 titleFormat.FontFamily = new Font("Times New Roman");
-                //Specify font size  
                 titleFormat.Size = 18D;
                 titleFormat.Position = 40;
                 titleFormat.FontColor = System.Drawing.Color.Black;
-                //titleFormat.UnderlineColor = System.Drawing.Color.Black; можно подчеркнуть по рофлу
-                // titleFormat.Italic = true; я не итальянец
                 Paragraph paragraphTitle = doc.InsertParagraph(title, false, titleFormat);
                 Paragraph paragraphTitle2 = doc.InsertParagraph(titlevar, false, titleFormat);
                 paragraphTitle.Alignment = Alignment.center;
                 paragraphTitle2.Alignment = Alignment.right;
                 for (int j = 0; j < variantList[i].tasks.Count; j++)
                 {
-                    //Text  
                     string textParagraph = (j + 1) + "." + variantList[i].tasks[j].conditionWithNumberedQuestions + '\r';
-                    //string otvet = (i + 1) + variantList[i].tasks[j].numberedAnswers;
-                    ////Formatting Title  
-                    //Formatting titleFormat = new Formatting();
-                    ////Specify font family  
-                    //titleFormat.FontFamily = new Font("Times New Roman");
-                    ////Specify font size  
-                    //titleFormat.Size = 18D;
-                    //titleFormat.Position = 40;
-                    //titleFormat.FontColor = System.Drawing.Color.Black;
-                    ////titleFormat.UnderlineColor = System.Drawing.Color.Black; можно подчеркнуть по рофлу
-                    //// titleFormat.Italic = true; я не итальянец
-                    //Specify font family  
                     titleFormat.FontFamily = new Font("Times New Roman");
-                    //Specify font size  
                     titleFormat.Size = 15D;
                     titleFormat.Position = 40;
                     titleFormat.FontColor = System.Drawing.Color.Black;
-                    //titleFormat.UnderlineColor = System.Drawing.Color.Black;
-                    //titleFormat.Italic = true;
-                    //Formatting Text Paragraph  
                     Formatting textParagraphFormat = new Formatting();
-                    //font family  
                     textParagraphFormat.FontFamily = new Font("Arial");
-                    //font size  
-                    textParagraphFormat.Size = 10D;
-                    //Spaces between characters  
-                    textParagraphFormat.Spacing = 1;
-                    //Create docx  
-                    //var doc = DocX.Create(pathdoc);
-                    //Insert title  
-                    //Paragraph paragraphTitle = doc.InsertParagraph(title, false, titleFormat);
-                    //Paragraph paragraphTitle2 = doc.InsertParagraph(titlevar, false, titleFormat);
-                    //paragraphTitle.Alignment = Alignment.center;
-                    //paragraphTitle2.Alignment = Alignment.right;
-                    //Insert text  
+                    textParagraphFormat.Spacing = 0;
                     doc.InsertParagraph(textParagraph, false, textParagraphFormat);
-
-                    //doc.InsertParagraph(otvet, false, textParagraphFormat);
-
-                    //Process.Start("WINWORD.EXE", pathdoc);
                 }
             }
             for (int i = 0; i < variantList.Count; i++)
